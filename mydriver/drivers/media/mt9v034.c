01340ab1e9f145a338ab68a2c4e5e18fda181f5a
ilikara <3435193369@qq.com>
2024-12-05 16:42:08 +0000

    incomplete isr and dma

--- mydriver/drivers/media/mt9v034.c
+++ mydriver/drivers/media/mt9v034.c
@@ -2,7 +2,7 @@
  * @Author: ilikara 3435193369@qq.com
  * @Date: 2024-12-05 08:02:57
  * @LastEditors: ilikara 3435193369@qq.com
- * @LastEditTime: 2024-12-05 13:40:54
+ * @LastEditTime: 2024-12-05 16:41:27
  * @FilePath: /ls2k0300_peripheral_library/mydriver/drivers/media/mt9v034.c
  * @Description:
  *
@@ -14,6 +14,7 @@
 #include <linux/platform_device.h>
 #include <linux/gpio/consumer.h>
 #include <linux/serial_core.h>
+#include <linux/dmaengine.h>
 #include <linux/tty.h>
 #include <linux/of.h>
 #include <linux/of_gpio.h>
@@ -31,7 +32,6 @@
 #define GTIM_CNT 0x24
 #define GTIM_PSC 0x28
 #define GTIM_ARR 0x2C
-#define GTIM_CCR1 0x34
 #define GTIM_CCR(n) 0x34 + (n) * 0x04
 #define GTIM_INSTA 0x50
 
@@ -68,22 +68,36 @@ struct mt9v034_camera
 {
     struct gpio_desc *gpio[10];
     struct platform_device *gpio_pdev, *uart_pdev;
+    struct dma_chan *dma_chan;
     void __iomem *mmio_base;
     u32 irq;
     u32 start_gpio;
     u32 depth;
+    u32 count, img_size;
+    u8 buffer[][], image[][], *it;
+};
+
+#define GPIO_BASE 0x16104000
+#define GPIO_I 0x20
+// 假设为GPIO04-11 vsync为GPIO 0
+static irqreturn_t
+mt9v034_isr(int irq, void *dev)
+{
+    struct mt9v034_camera *cam = (struct mt9v034_camera *)dev;
+    *cam->it = (readl(GPIO_BASE + GPIO_I + 0x07) >> cam->start_gpio);
+    cam->it++;
+    cam->count++;
+    if (!(readl(GPIO_BASE + GPIO_I + 0x07) & BIT(0)) || cam->count > cam->img_size)
+    {
+        cam->it = cam->buffer;
+        memcpy(cam->image, cam->buffer, cam->img_size);
+    }
+    readl(cam->mmio_base + GTIM_CCR(CAM_GTIM_CH));
+    return IRQ_HANDLED;
 }
 
 // data-pins = <&gpa0 4 8 GPIO_ACTIVE_HIGH>;
 // uart = <&cpu_uart1>;
-
-static irqreturn_t
-mt9v034_isr(int irq, void *dev)
-{
-
-    return IRQ_HANDLED;
-}
-
 static int
 mt9v034_probe(struct platform_device *pdev)
 {
@@ -155,7 +169,33 @@ mt9v034_probe(struct platform_device *pdev)
     // uart_write(port, (u_char *));
     dev_info(&pdev->dev, "GPIO range: start=%d, count=%d\n", start_gpio, ngpios);
 
-    // 启用GTIM_CH3的捕获中断，通道号定义在CAM_GTIM_CH
+    // DMA初始化
+    cam->dma_chan = dma_request_slave_channel(&pdev->dev, "CAM_PCLK");
+    if (!dma_chan)
+    {
+        dev_err(&pdev->dev, "Failed to request DMA channel\n");
+        return -ENODEV;
+    }
+    struct dma_slave_config config = {
+        .direction = DMA_FROM_DEVICE,                // 或者 DMA_TO_DEVICE
+        .src_addr = GPIO_BASE + GPIO_I + 0x07,       // 使用GPIO00-07
+        .dst_addr = cam->buffer,                     // 目标地址
+        .src_addr_width = DMA_SLAVE_BUSWIDTH_1_BYTE, // 源地址宽度
+        .dst_addr_width = DMA_SLAVE_BUSWIDTH_1_BYTE, // 目标地址宽度
+        .src_maxburst = 16,                          // 源端最大突发长度
+        .dst_maxburst = 16,                          // 目标端最大突发长度
+        .device_fc = false,                          // 是否启用流控
+        .slave_id = 0,                               // 如果需要指定从设备ID
+    };
+    if (dmaengine_slave_config(cam->dma_chan, &config))
+    {
+        dev_err(dev, "Failed to configure DMA channel\n");
+        return -EINVAL;
+    }
+    struct scatterlist sg;
+    sg_init_table
+
+    // 为PCLK启用GTIM_CH3的捕获中断，通道号定义在CAM_GTIM_CH
 
     u32 ccmr_reg = readl(cam->mmio_base + GTIM_CCMR(CAM_GTIM_CH));
     ccmr_reg &= ~(0b11 * CCMR_CCnS(CAM_GTIM_CH));
@@ -171,19 +211,18 @@ mt9v034_probe(struct platform_device *pdev)
     dier_reg |= DIER_CCnIE(CAM_GTIM_CH); // 中断使能
     writel(dier_reg, cam->mmio_base + GTIM_DIER);
 
-    // 中断回调 需配置 interrupts=<26>;
-    // 26为GTIM的中断号
-    u32 irq;
+    // 为Vsync启用GPIO中断 下降沿触发
 
-    irq = platform_get_irq(pdev, 0);
-    if (irq <= 0)
+    // 配置中断回调 需配置 interrupts=<26>;
+    // 26为GTIM的中断号
+    cam->irq = platform_get_irq(pdev, 0);
+    if (cam->irq <= 0)
     {
         dev_err(&pdev->dev, "no irq resource?\n");
         return -ENODEV;
     }
-    cam->irq = irq;
 
-    err = request_irq(cam->irq, mt9v034_isr, IRQF_TRIGGER_RISING, "camera_interrupts", cam);
+    err = request_irq(cam->irq, mt9v034_isr, IRQF_TRIGGER_RISING, "camera_interrupts", cam); // 不确定
 
     if (err)
         dev_err(&pdev->dev, "failure requesting irq %d\n", err);
